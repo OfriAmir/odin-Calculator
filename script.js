e43ad73a37175ef0a6778eebf95d56fcd9780d24
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-06-05 22:46:39 +0300

    Finish calculator with a few bugs to fix

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ function divide(a, b){
     return a / b
 }
 
-let firstNum = 0
-let secondNum = 0
-let operator
-
+let firstNum = ""
+let secondNum = ""
+let operator = ""
+let allTogether
+let result
 function operate(a, op, b){
+    a = +a
+    b = +b
     switch (op){
         case "+":
             return add(a, b);
@@ -31,13 +34,20 @@ function operate(a, op, b){
             break;
     }
 }
-
+function isTooLong(str) {
+    return str.length > 9
+}
+function roundToNinthDigit(str) {
+    const lastTwo = +str.slice(9, 11)
+    const rounded = Math.round(lastTwo / 10)
+    return str.slice(0, 8) + rounded.toString()
+}
 const calculator = document.querySelector(".calculator")
 const btnsDiv = document.querySelector(".btns-div")
+const displayDiv = document.querySelector(".display-div")
 const buttons = []
 const buttonsDiv = []
-const buttonsText = "c+-:123x456-789+0.="
-const j = 1
+const buttonsText = "c  :123x456-789+0.="
 for (let i = 0; i < buttonsText.length; i++){
     buttons[i] = document.createElement("button")
     buttons[i].classList.toggle("calc-btns")
@@ -48,4 +58,35 @@ for (let i = 0; i < buttonsText.length; i++){
         btnsDiv.appendChild(buttonsDiv[i/4])
     }
     buttonsDiv[Math.floor([i/4])].appendChild(buttons[i])
+    buttons[i].addEventListener("click", (e) => {
+        if (e.target.textContent == "c" || (result)){
+            firstNum = ""
+            operator = ""
+            secondNum = ""
+            displayDiv.textContent = "0"
+            result = 0
+        }
+        if (!(firstNum.length == 6 || secondNum.length == 6)) {
+            if (!operator && ["0","1","2","3","4","5","6","7","8","9"].includes(e.target.textContent)) firstNum += e.target.textContent;
+            if (firstNum && !operator && ["+","-",":","x"].includes(e.target.textContent)) operator = e.target.textContent 
+            if (operator && ["0","1","2","3","4","5","6","7","8","9"].includes(e.target.textContent)) secondNum += e.target.textContent
+            if (secondNum && ["+","-",":","x"].includes(e.target.textContent)) {
+                firstNum = operate(firstNum, operator, secondNum)
+                operator = e.target.textContent
+                secondNum = ""
+            }
+        }   
+        if (secondNum && e.target.textContent == "="){
+            result = operate(firstNum, operator, secondNum).toString()
+            if (isTooLong(result)) displayDiv.textContent = roundToNinthDigit(result)
+            else displayDiv.textContent = result
+        } else if (firstNum) {
+            allTogether = firstNum + operator + secondNum
+            if (isTooLong(allTogether)) displayDiv.textContent = roundToNinthDigit(allTogether)
+            else displayDiv.textContent = allTogether
+        }
+
+        
+        //displayDiv.textContent = firstNum + operator + secondNum
+    })
 }
