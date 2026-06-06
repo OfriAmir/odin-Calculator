eab55cb7cd60239bf1602379175235ae117e9b0f
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-06-06 21:38:38 +0300

    Add keyboard support and more CSS

--- script.js
+++ script.js
@@ -48,23 +48,38 @@ function includesOperators (str) {
 //variables
 let firstNum = ""
 let firstNumPoint
+let preFirstNum
 let secondNum = ""
-let secondNumPoint 
+let secondNumPoint
+let preSecondNum 
 let operator = ""
+let preOperator
 let allTogether
 let result
+let currentDisplay
+const body = document.querySelector("body")
 const calculator = document.querySelector(".calculator")
 const btnsDiv = document.querySelector(".btns-div")
 const displayDiv = document.querySelector(".display-div")
 const buttons = []
 const buttonsDiv = []
-const buttonsText = "c< :123x456-789+0.="
+const buttonsText = "c<p:123x456-789+0.="
+const buttonsTextNoSpace = "c<p:123x456-789+0.="
 
 //Create desired HTML layout 
 for (let i = 0; i < buttonsText.length; i++){
     buttons[i] = document.createElement("button")
     buttons[i].classList.toggle("calc-btns")
     buttons[i].textContent = buttonsText[i]
+    if (buttonsText[i] === "p"){
+        buttons[i].textContent = "pre"
+    }
+    if (buttonsText[i] === "="){
+        buttons[i].style.maxWidth = "1000px"
+        buttons[i].style.flexGrow = "2"
+        buttons[i].style.backgroundColor = "white"
+    }
+    if (includesOperators(buttonsText[i])) buttons[i].style.backgroundColor = "rgb(255, 174, 0)"
     //add a row div every 4 buttons
     if (i % 4 == 0){
         buttonsDiv[i/4] = document.createElement("div")
@@ -76,11 +91,19 @@ for (let i = 0; i < buttonsText.length; i++){
     buttons[i].addEventListener("click", doEverything)
 }
 
+body.addEventListener("keydown", (e) => {
+    if (buttonsTextNoSpace.includes(e.key)) doEverything(e)
+})
+
+
 
 
 function doEverything (e) {
-    //cases that involve clearing data
-    if (e.target.textContent == "c"){
+    //click or keydown
+    if (e.key) currentDisplay = e.key
+    else currentDisplay = e.target.textContent
+    //special cases that involve clearing or changing data
+    if (currentDisplay == "c"){
         firstNum = ""
         firstNumPoint = false
         operator = ""
@@ -89,9 +112,8 @@ function doEverything (e) {
         displayDiv.textContent = "0"
         result = null
     }
-    if ((result || result === 0) && (includesDigits(e.target.textContent) ||
-        e.target.textContent === "." ||
-        e.target.textContent === "<")){
+    if ((result || result === 0) && (includesDigits(currentDisplay) ||
+        currentDisplay === ".")){
         firstNum = ""
         firstNumPoint = false
         operator = ""
@@ -100,7 +122,8 @@ function doEverything (e) {
         displayDiv.textContent = "0"
         result = null
     }
-    if (result && includesOperators(e.target.textContent)){
+    if ((result || result === 0) && (includesOperators(currentDisplay)||
+        currentDisplay === "<")){
         firstNum = result.toString()
         operator = ""
         secondNum = ""
@@ -108,14 +131,20 @@ function doEverything (e) {
         result = null
     }
 
+    if (currentDisplay === "p"){
+        [preFirstNum, firstNum] = [firstNum, preFirstNum];
+        [preOperator, operator] = [operator, preOperator];
+        [preSecondNum, secondNum] = [secondNum, preSecondNum];
+        result = null
+    }
 
     // Cases that involve adding data
-    if (!operator && includesDigits(e.target.textContent)){
-        firstNum += e.target.textContent;
+    if (!operator && includesDigits(currentDisplay)){
+        firstNum += currentDisplay;
     }
-    if (firstNum && !secondNum && includesOperators(e.target.textContent)) operator = e.target.textContent 
-    if (operator && includesDigits(e.target.textContent)) secondNum += e.target.textContent
-    if (e.target.textContent === "."){
+    if (firstNum && !secondNum && includesOperators(currentDisplay)) operator = currentDisplay 
+    if (operator && includesDigits(currentDisplay)) secondNum += currentDisplay
+    if (currentDisplay === "."){
         if (!firstNumPoint && !operator){
             firstNum += "."
             firstNumPoint = true
@@ -124,15 +153,18 @@ function doEverything (e) {
             secondNumPoint = true
         }
     }
-    if (e.target.textContent === "<"){
+    if (currentDisplay === "<"){
         if (secondNum) secondNum = secondNum.slice(0, -1)
         else if (operator) operator = ""
         else if (firstNum) firstNum = firstNum.slice(0, -1)
     }
-    if (secondNum && includesOperators(e.target.textContent)) {
+    if (secondNum && includesOperators(currentDisplay)) {
         // toPrecision(12 specifically) is to avoid 0.6*3 = 1.7999999... (12 digits) and parseFloat it to remove 0s
+        preFirstNum = firstNum
+        preOperator = operator
+        preSecondNum = secondNum
         firstNum = parseFloat((operate(firstNum, operator, secondNum)).toPrecision(12))
-        operator = e.target.textContent
+        operator = currentDisplay
         secondNum = ""
     }
 
@@ -144,14 +176,17 @@ function doEverything (e) {
     if (isTooLong((allTogether))) {
         displayDiv.textContent = "17+ TOO LONG"
         
-    } else if (secondNum && e.target.textContent == "="){
+    } else if (secondNum && currentDisplay == "="){
+        preFirstNum = firstNum
+        preOperator = operator
+        preSecondNum = secondNum
         result = operate(firstNum, operator, secondNum)
         displayDiv.textContent = parseFloat(result.toPrecision(12))
     } else if (firstNum) {
         displayDiv.textContent = allTogether
     } else displayDiv.textContent = "0"
 
-    if ((displayDiv.textContent).length > 9){
+    if ((displayDiv.textContent).length > 8){
         displayDiv.style.overflowX = "scroll"
     } else {
         displayDiv.style.overflowX = ""
